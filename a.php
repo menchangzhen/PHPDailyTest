069c4df2192a2b3224175ebd6e517de08c5ca4c4
zhen震zhen <1131274908@qq.com>
2019-05-16 08:45:42 +0800

    Signed-off-by: zhen震zhen <1131274908@qq.com>

---
+++ a.php
@@ -0,0 +1,16 @@
+<?php 
+$n =1;
+$m = 13;
+print_r(calFn($n,$m));
+function calFn($x,$y)
+{
+  $arr = [];
+  for ($i=$x; $i <=$y ; $i++) { 
+  	$arr[] = substr_count($i,1);
+  }
+  return array_sum($arr);
+}
+
+
+
+ ?>
