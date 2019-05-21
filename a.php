a27f9abbee9ef8b66ba8846c53e9eb280544904c
zhen震zhen <1131274908@qq.com>
2019-05-21 08:47:19 +0800

    Signed-off-by: zhen震zhen <1131274908@qq.com>

--- a.php
+++ a.php
@@ -1,14 +1,18 @@
 <?php 
-$n =1;
-$m = 13;
-print_r(calFn($n,$m));
-function calFn($x,$y)
+$array = [2,4,3,6,3,2,5,5];
+print_r(FindNumsAppearOnce($array));
+function FindNumsAppearOnce($array)
 {
-  $arr = [];
-  for ($i=$x; $i <=$y ; $i++) { 
-  	$arr[] = substr_count($i,1);
-  }
-  return array_sum($arr);
+$str = [];
+$data = array_count_values($array);
+foreach ($data as $key => $value) {
+	if($value == 1)
+	{
+		$str[] = $key;
+	}
+}
+print_r($str);
+
 }
 
 
